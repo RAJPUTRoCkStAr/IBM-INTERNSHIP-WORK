e94cd6effd89bc5f48a538cc061ca83ed5f51a40
unknown <sumitsingh9441@gmail.com>
2024-07-15 17:18:10 +0530

    some

--- Class/app.py
+++ Class/app.py
@@ -1,2 +1,19 @@
-import pandas as pd
-import streamlit as st
+import pandas as pd;
+import joblib ;
+import streamlit as st;
+
+model = joblib.load('mj')
+
+st.title(" House Price Prediction")
+
+
+
+area = st.number_input("Enter the Area")
+# bedroom = st.number_input("Enter the bedroom")
+
+def prediction(area):
+    P= model.predict([[area]])
+    return P
+if(st.button('predict')):
+    result = prediction(area)
+    st.success("The predicted price is {}".format(result))
